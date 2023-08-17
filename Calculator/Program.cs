60a01085bd7d05f15dbea75de4777fdb41e5b2ef
Dan Boudet <dboudet@smartxadvisory.com>
2023-08-17 01:16:29 -0400

    Section 2:47

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -4,7 +4,7 @@ Console.WriteLine("Input the first number:");
 int? firstNum = int.Parse(Console.ReadLine());
 
 Console.WriteLine("Input the second number:");
-int? secondtNum = int.Parse(Console.ReadLine());
+int? secondNum = int.Parse(Console.ReadLine());
 
 Console.WriteLine("What do you want to do with those numbers?");
 Console.WriteLine("[A]dd");
@@ -13,14 +13,45 @@ Console.WriteLine("[M]ultiply");
 
 string? operationInput = Console.ReadLine()?.ToUpper();
 
-Console.WriteLine(ConstructFormula(firstNum, secondtNum, operationInput));
 
-string ConstructFormula(int? num1, int? num2, string? operation)
+// My original approach
+//Console.WriteLine(ConstructFormula(firstNum, secondNum, operationInput));
+
+//string ConstructFormula(int? num1, int? num2, string? operation)
+//{
+//    if (operation == "A") return num1 + " + " + num2 + " = " + (num1 + num2);
+//    else if (operation == "S") return num1 + " - " + num2 + " = " + (num1 - num2);
+//    else if (operation == "M") return num1 + " * " + num2 + " = " + (num1 * num2);
+//    else return "Invalid option";
+//}
+
+// her approach
+if (EqualsCaseInsensitive(operationInput, "A"))
 {
-    if (operation == "A") return num1 + " + " + num2 + " = " + (num1 + num2);
-    if (operation == "S") return num1 + " - " + num2 + " = " + (num1 - num2);
-    if (operation == "M") return num1 + " * " + num2 + " = " + (num1 * num2);
-    else return "Invalid option";
+    var sum = firstNum + secondNum;
+    PrintFinalEquation(firstNum, secondNum, sum, "+");
+}
+else if (EqualsCaseInsensitive(operationInput, "S"))
+{
+    var diff = firstNum - secondNum;
+    PrintFinalEquation(firstNum, secondNum, diff, "-");
+}
+else if (EqualsCaseInsensitive(operationInput, "M"))
+{
+    var mult = firstNum * secondNum;
+    PrintFinalEquation(firstNum, secondNum, mult, "*");
+}
+else Console.WriteLine("Invalid option");
+
+void PrintFinalEquation(int? num1, int? num2, int? result, string @operator)
+{
+    //Console.WriteLine(num1 + " " + @operator + num2 + " = " + result);
+    Console.WriteLine($"{num1} {@operator} {num2} = {result}");
+}
+
+bool EqualsCaseInsensitive(string str1, string str2)
+{
+    return str1.ToUpper() == str2;
 }
 
 
--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -1,55 +1,86 @@
-﻿Console.WriteLine("What do you want to do?");
-Console.WriteLine("[S]ee all todos");
-Console.WriteLine("[A]dd a todo");
-Console.WriteLine("[R]emove a todo");
-Console.WriteLine("[E]xit");
+﻿
+bool stopProgram = false;
 
-// TODO: handle user input
-/*
- * this is a 
- * multiline
- * comment
-*/
-
-string? userChoice = Console.ReadLine();
-
-switch (userChoice)
+do
 {
-    case "S":
-        PrintSelectedOption("See all todos");
-        break;
-    case "A":
-        PrintSelectedOption("Add a todo");
-        break;
-    case "R":
-        PrintSelectedOption("Remove a todo");
-        break;
-    case "E":
-        PrintSelectedOption("Exit");
-        break;
-    default: Console.WriteLine("Invalid Input");
-        break;
-}
+    Console.WriteLine("What do you want to do?");
+    Console.WriteLine("[S]ee all todos");
+    Console.WriteLine("[A]dd a todo");
+    Console.WriteLine("[R]emove a todo");
+    Console.WriteLine("[E]xit");
 
-//if (userChoice == "S")
-//{
-//    PrintSelectedOption("See all todos");
-//}
-//else if (userChoice == "A")
-//{
-//    PrintSelectedOption("Add a todo");
-//}
-//else if (userChoice == "R")
-//{
-//    PrintSelectedOption("Remove a todo");
-//}
-//else if (userChoice == "E")
-//{
-//    PrintSelectedOption("Exit");
-//}
-Console.ReadKey();
+    //string? userChoice = Console.ReadLine();
+    string? userChoice = Console.ReadLine().ToUpper();
+
+    if (userChoice == "S")
+    {
+        PrintSelectedOption("See all todos");
+    }
+    else if (userChoice == "A")
+    {
+        PrintSelectedOption("Add a todo");
+    }
+    else if (userChoice == "R")
+    {
+        PrintSelectedOption("Remove a todo");
+    }
+    else if (userChoice == "E")
+    {
+        PrintSelectedOption("Exit");
+        handleStopProgram();
+        break;
+    }
+    else Console.WriteLine("Invalid Input");
+
+    Console.WriteLine("Would you like to continue? [Y/N]");
+    string? userContinueInput = Console.ReadLine().ToUpper();
+
+    if (userContinueInput != "Y")
+    {
+        handleStopProgram();
+    }
+}
+while (stopProgram == false);
+
+
+
+//Console.ReadKey();
 
 void PrintSelectedOption(string selectedOption)
 {
     Console.WriteLine("Selected option: " + selectedOption);
 }
+
+void handleStopProgram()
+{
+    stopProgram = true;
+    Console.WriteLine("Thank you. Press any key to close.");
+    Console.ReadKey();
+}
+
+
+
+// her approach
+//switch (userChoice)
+//{
+//    case "S":
+//    case "s":
+//        PrintSelectedOption("See all todos");
+//        break;
+//    case "A":
+//    case "a":
+//        PrintSelectedOption("Add a todo");
+//        break;
+//    case "R":
+//    case "r":
+//        PrintSelectedOption("Remove a todo");
+//        break;
+//    case "E":
+//    case "e":
+//        PrintSelectedOption("Exit");
+//        break;
+//    default:
+//        Console.WriteLine("Invalid Input");
+//        break;
+//}
+
