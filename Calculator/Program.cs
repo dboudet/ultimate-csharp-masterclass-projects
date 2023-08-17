267575ad5450ebd087e6bcc10557c9c68c8020c6
Dan Boudet <dboudet@smartxadvisory.com>
2023-08-16 22:22:57 -0400

    initial commit with calculator

---
+++ Calculator/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Hello!");
+
+Console.WriteLine("Input the first number:");
+int? firstNum = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Input the second number:");
+int? secondtNum = int.Parse(Console.ReadLine());
+
+Console.WriteLine("What do you want to do with those numbers?");
+Console.WriteLine("[A]dd");
+Console.WriteLine("[S]ubtract");
+Console.WriteLine("[M]ultiply");
+
+string? operationInput = Console.ReadLine()?.ToUpper();
+
+Console.WriteLine(ConstructFormula(firstNum, secondtNum, operationInput));
+
+string ConstructFormula(int? num1, int? num2, string? operation)
+{
+    if (operation == "A") return num1 + " + " + num2 + " = " + (num1 + num2);
+    if (operation == "S") return num1 + " - " + num2 + " = " + (num1 - num2);
+    if (operation == "M") return num1 + " * " + num2 + " = " + (num1 * num2);
+    else return "Invalid option";
+}
+
+
+
+Console.WriteLine("Press any key to close");
+Console.ReadKey();
---
+++ TodoList/Program.cs
@@ -0,0 +1,55 @@
+﻿Console.WriteLine("What do you want to do?");
+Console.WriteLine("[S]ee all todos");
+Console.WriteLine("[A]dd a todo");
+Console.WriteLine("[R]emove a todo");
+Console.WriteLine("[E]xit");
+
+// TODO: handle user input
+/*
+ * this is a 
+ * multiline
+ * comment
+*/
+
+string? userChoice = Console.ReadLine();
+
+switch (userChoice)
+{
+    case "S":
+        PrintSelectedOption("See all todos");
+        break;
+    case "A":
+        PrintSelectedOption("Add a todo");
+        break;
+    case "R":
+        PrintSelectedOption("Remove a todo");
+        break;
+    case "E":
+        PrintSelectedOption("Exit");
+        break;
+    default: Console.WriteLine("Invalid Input");
+        break;
+}
+
+//if (userChoice == "S")
+//{
+//    PrintSelectedOption("See all todos");
+//}
+//else if (userChoice == "A")
+//{
+//    PrintSelectedOption("Add a todo");
+//}
+//else if (userChoice == "R")
+//{
+//    PrintSelectedOption("Remove a todo");
+//}
+//else if (userChoice == "E")
+//{
+//    PrintSelectedOption("Exit");
+//}
+Console.ReadKey();
+
+void PrintSelectedOption(string selectedOption)
+{
+    Console.WriteLine("Selected option: " + selectedOption);
+}
