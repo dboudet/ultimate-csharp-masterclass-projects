08c5bec2d0c3c71eb1a68ea10b3944b2de27db69
Dan Boudet <dboudet@smartxadvisory.com>
2023-08-29 16:44:50 -0400

    v2 of dice roll game - started over with more OOP approach

---
+++ DiceRollGameV2/Dice.cs
@@ -0,0 +1,15 @@
+﻿public class Dice
+{
+    readonly int _numberOfSides;
+    public Dice(int numberOfSides = 6)
+    {
+        _numberOfSides = numberOfSides;
+    }
+
+    public int Roll()
+    {
+        var _random = new Random();
+        // lower bound inclusive; upper bound exclusive
+        return _random.Next(1, _numberOfSides + 1);
+    }
+}
---
+++ DiceRollGameV2/Program.cs
@@ -0,0 +1,35 @@
+﻿
+bool isCorrectGuess = false;
+int remainingGuesses = 3;
+
+// can change dice sides; defaults to 6 in Constructor
+var diceRoll = new Dice().Roll();
+DisplayMessage.Intro();
+
+
+while (remainingGuesses > 0 && !isCorrectGuess)
+{
+    var userInput = new UserInput(Console.ReadLine());
+
+    if (!userInput.IsInteger)
+    {
+        DisplayMessage.Invalid();
+    }
+    else if (userInput.InputAsInt == diceRoll)
+    {
+        DisplayMessage.Winner();
+        isCorrectGuess = true;
+        break;
+    }
+    else
+    {
+        DisplayMessage.WrongValidGuess();
+        remainingGuesses--;
+    }
+    
+    if (remainingGuesses > 0) DisplayMessage.EnterNumber();
+}
+
+if (!isCorrectGuess) DisplayMessage.Loser();
+
+Console.ReadKey();
