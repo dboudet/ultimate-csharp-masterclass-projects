a8b0b84927601e058e8a2f3f68a2d0c9c9393e93
Dan Boudet <dboudet@smartxadvisory.com>
2023-08-30 12:55:01 -0400

    handle invalid inputs for max guesses and dice sides

--- DiceRollGameV2/Dice.cs
+++ DiceRollGameV2/Dice.cs
@@ -3,7 +3,15 @@
     readonly int _numberOfSides;
     public Dice(int numberOfSides)
     {
+        if (numberOfSides > 0)
+        {
         _numberOfSides = numberOfSides;
+        }
+        else
+        {
+            Console.WriteLine("Invalid number of sides provided. Using default of 6");
+            _numberOfSides = 6;
+        }
     }
 
     public int Roll()
--- DiceRollGameV2/GameMechanics.cs
+++ DiceRollGameV2/GameMechanics.cs
@@ -3,6 +3,6 @@
     public bool IsCorrectGuess = false;
     public int RemainingGuesses;
     public int DiceSides { get; init; } = 6;
-    public int MaxGuesses { init => RemainingGuesses = value; }
+    public int MaxGuesses { init => RemainingGuesses = value > 0 ? value : 3; }
     public bool IsNotOver() => RemainingGuesses > 0 && !IsCorrectGuess;
 }
