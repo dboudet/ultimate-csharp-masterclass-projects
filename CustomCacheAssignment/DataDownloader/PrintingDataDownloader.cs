20d6f13c1b7a6e5a91d1ca66e746b54969a5a596
Dan Boudet <dboudet@smartxadvisory.com>
2023-09-28 09:20:18 -0400

    move printingDataDownloader to new file

---
+++ CustomCacheAssignment/DataDownloader/PrintingDataDownloader.cs
@@ -0,0 +1,19 @@
+﻿namespace CustomCacheAssignment.DataDownloader
+{
+    public class PrintingDataDownloader<TKey, TData> : IDataDownloader<TKey, TData>
+    {
+        private readonly IDataDownloader<TKey, TData> _dataDownloader;
+
+        public PrintingDataDownloader(IDataDownloader<TKey, TData> dataDownloader)
+        {
+            _dataDownloader = dataDownloader;
+        }
+
+        public TData DownloadData(TKey resourceId)
+        {
+            var data = _dataDownloader.DownloadData(resourceId);
+            Console.WriteLine("Data is ready!");
+            return data;
+        }
+    }
+}
--- CustomCacheAssignment/DataDownloader/SlowDataDownloader.cs
+++ CustomCacheAssignment/DataDownloader/SlowDataDownloader.cs
@@ -12,21 +12,4 @@
                 typeof(TData));
         }
     }
-
-    public class PrintingDataDownloader<TKey, TData> : IDataDownloader<TKey, TData>
-    {
-        private readonly IDataDownloader<TKey, TData> _dataDownloader;
-
-        public PrintingDataDownloader(IDataDownloader<TKey, TData> dataDownloader)
-        {
-            _dataDownloader = dataDownloader;
-        }
-
-        public TData DownloadData(TKey resourceId)
-        {
-            var data = _dataDownloader.DownloadData(resourceId);
-            Console.WriteLine("Data is ready!");
-            return data;
-        }
-    }
 }
