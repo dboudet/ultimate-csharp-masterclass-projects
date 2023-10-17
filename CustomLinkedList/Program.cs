0858b26178327d6010a150d87856067886f24fad
Dan Boudet <dboudet@smartxadvisory.com>
2023-10-16 22:06:29 -0400

    Custom Linked List - section 10 assignment

---
+++ CustomLinkedList/Program.cs
@@ -0,0 +1,207 @@
+﻿using System.Collections;
+
+
+var myLinkedList = new CustomLinkedList<string>();
+
+myLinkedList.Add("a");
+myLinkedList.AddToEnd("b");
+myLinkedList.AddToFront("c");
+
+foreach (var item in myLinkedList) Console.WriteLine(item);
+myLinkedList.Remove("a");
+Console.WriteLine(myLinkedList);
+
+
+
+
+
+Console.ReadKey();
+
+public interface ILinkedList<T> : ICollection<T>
+{
+    void AddToFront(T item);
+    void AddToEnd(T item);
+}
+
+public class CustomLinkedList<T> : ILinkedList<T?>
+{
+    public bool IsReadOnly { get; } = false;
+    private CustomLinkedListNode<T>? _head;
+    private CustomLinkedListNode<T>? _tail;
+    private int _count;
+    public int Count => _count;
+
+    public CustomLinkedList()
+    {
+    }
+
+    //public int GetCount()
+    //{
+    //    int count = 0;
+    //    //while ()
+    //    return count;
+    //}
+    public void Add(T? item) => AddToEnd(item);
+
+    public void AddToEnd(T? item)
+    {
+        var newItem = new CustomLinkedListNode<T>(this, item);
+
+        if (_head is null)
+        {
+            AddToEmpty(newItem);
+        }
+        else
+        {
+            _tail!.Next = newItem;
+            _tail = newItem;
+            newItem.Next = null;
+        }
+        _count++;
+    }
+
+    public void AddToFront(T? item)
+    {
+        var newItem = new CustomLinkedListNode<T>(this, item);
+
+        if (_head is null)
+        {
+            AddToEmpty(newItem);
+        }
+        else
+        {
+            newItem.Next = _head;
+            _head = newItem;
+        }
+        _count++;
+    }
+
+    public void AddToEmpty(CustomLinkedListNode<T> newNode)
+    {
+        _head = newNode;
+        _tail = newNode;
+        newNode.Next = newNode;
+    }
+    public void Clear()
+    {
+        _head = null;
+        _tail = null;
+        _count = 0;
+    }
+
+    public bool Contains(T? value) => Find(value) is not null;
+    public CustomLinkedListNode<T>? Find(T? value)
+    {
+        CustomLinkedListNode<T>? node = _head;
+        if (node is null || node.Value is null) return null;
+        do
+        {
+            if (node.Value!.Equals(value)) return node;
+            node = node.Next;
+        }
+        while (node!.Next != _head);
+        return null;
+    }
+
+    public CustomLinkedListNode<T>? FindPrevious(T? value)
+    {
+        CustomLinkedListNode<T>? node = _head;
+        if (node is null || node.Value is null || node.Next is null) return null;
+        do
+        {
+            if (node.Next!.Value!.Equals(value)) return node;
+            node = node.Next;
+        }
+        while (node != _head);
+        return null;
+    }
+
+    public void CopyTo(T?[] array, int arrayIndex)
+    {
+        if (arrayIndex < 0 || arrayIndex >= array.Length)
+            throw new ArgumentOutOfRangeException("index out of range");
+
+        CustomLinkedListNode<T> node = _head;
+        do
+        {
+            array[arrayIndex] = node.Value;
+            node = node.Next;
+        }
+        while (node != _head);
+    }
+
+    public CustomEnumerator GetEnumerator()
+    {
+        return new CustomEnumerator(this);
+    }
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+    IEnumerator<T> IEnumerable<T?>.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public bool Remove(T? item)
+    {
+        CustomLinkedListNode<T>? itemToRemove = Find(item);
+        if (itemToRemove == null) return false;
+        else
+        {
+            CustomLinkedListNode<T>? prevItem = FindPrevious(item);
+            if (prevItem is not null) prevItem.Next = itemToRemove.Next;
+            itemToRemove.Next = null;
+            _count--;
+            return true;
+        }
+    }
+
+    public struct CustomEnumerator : IEnumerator<T>, IEnumerator
+    {
+        private readonly CustomLinkedList<T> _customLinkedList;
+        private readonly int initialIndex = -1;
+        private int _currentIndex;
+        private T? _current;
+
+        public CustomEnumerator(CustomLinkedList<T> linkedList)
+        {
+            _customLinkedList = linkedList;
+        }
+
+        public T Current => _current!;
+
+        object? IEnumerator.Current => _current;
+
+        public bool MoveNext()
+        {
+            ++_currentIndex;
+            return _currentIndex < _customLinkedList.Count;
+        }
+
+        public void Reset() => _currentIndex = initialIndex;
+        public void Dispose()
+        {
+        }
+    }
+}
+
+public class CustomLinkedListNode<T>
+{
+    public CustomLinkedList<T>? NodeList;
+    public T? Value;
+    public CustomLinkedListNode<T>? Next;
+
+    public CustomLinkedListNode(CustomLinkedList<T> nodeList, T value)
+    {
+        NodeList = nodeList;
+        Value = value;
+    }
+
+    public override string ToString()
+    {
+        return $"Value: {Value}" +
+            $"Next: {(Next is null ? "null" : Next.Value)}";
+    }
+}
+
---
+++ CustomLinkedListHerVersion/Program.cs
@@ -0,0 +1,180 @@
+﻿using System.Collections;
+
+
+var myLinkedList = new CustomLinkedList<string>();
+
+myLinkedList.Add("b");
+myLinkedList.AddToEnd("c");
+myLinkedList.AddToFront("a");
+
+foreach (var item in myLinkedList)
+{
+    Console.WriteLine(item);
+}
+Console.WriteLine(new String('-', 30));
+Console.WriteLine("Contains b?" + myLinkedList.Contains("b"));
+Console.WriteLine("Contains d?" + myLinkedList.Contains("d"));
+myLinkedList.Remove("a");
+Console.WriteLine(new String('-', 30));
+foreach (var item in myLinkedList)
+{
+    Console.WriteLine(item);
+}
+
+
+var newArray = new string[7];
+//myLinkedList.CopyTo(newArray, 7);
+myLinkedList.CopyTo(newArray, 2);
+
+
+
+Console.ReadKey();
+
+public interface ILinkedList<T> : ICollection<T>
+{
+    void AddToFront(T item);
+    void AddToEnd(T item);
+}
+
+public class CustomLinkedList<T> : ILinkedList<T?>
+{
+    public bool IsReadOnly { get; } = false;
+    private CustomLinkedListNode? _head;
+    //private CustomLinkedListNode? _tail;
+    private int _count;
+    public int Count => _count;
+
+
+    public void Add(T? item) => AddToEnd(item);
+
+    public void AddToEnd(T? item)
+    {
+        // IMPORTANT: NOT ASSIGNING A NEXT VALUE TO LAST ITEM
+        // SO... LAST NEVER HAS NEXT - EASY TO KNOW WHEN ON LAST
+        var newNode = new CustomLinkedListNode(item);
+        if (_head is null)
+        {
+            _head = newNode;
+        }
+        else
+        {
+            var tail = GetNodes().Last();
+            tail.Next = newNode;
+        }
+        ++_count;
+    }
+
+    public void AddToFront(T? item)
+    {
+        var newHead = new CustomLinkedListNode(item)
+        {
+            Next = _head
+        };
+        _head = newHead;
+        ++_count;
+    }
+
+    public void Clear()
+    {
+        CustomLinkedListNode? current = _head;
+        while (current is not null)
+        {
+            CustomLinkedListNode? temp = current;
+            current = current.Next;
+            temp.Next = null;
+        }
+        _head = null;
+        _count = 0;
+    }
+
+    public bool Contains(T? item)
+    {
+        if (item is null)
+        {
+            return GetNodes().Any(x => x.Value is null);
+        }
+        return GetNodes().Any(x => item.Equals(x.Value));
+    }
+
+    public void CopyTo(T?[] array, int arrayIndex)
+    {
+        if (array is null)
+            throw new ArgumentNullException(nameof(array));
+        if (arrayIndex < 0 || arrayIndex >= array.Length)
+            throw new ArgumentOutOfRangeException(nameof(arrayIndex));
+        if (array.Length < _count + arrayIndex)
+            throw new ArgumentException("Array is not long enough.");
+        foreach (var node in GetNodes())
+        {
+            array[arrayIndex] = node.Value;
+            ++arrayIndex;
+        }
+    }
+
+    public bool Remove(T? item)
+    {
+        foreach (var node in GetNodes())
+        {
+            CustomLinkedListNode? predecessor = null;
+            if (
+                (node.Value is null && item is null) ||
+                (node.Value is not null && node.Value.Equals(item)))
+            {
+                if (predecessor is null)
+                {
+                    _head = node.Next;
+                }
+                else
+                {
+                    predecessor.Next = node.Next;
+
+                }
+                //node.Next = null;
+                --_count;
+                return true;
+            }
+            predecessor = node;
+        }
+        return false;
+    }
+
+    public IEnumerator<T?> GetEnumerator()
+    {
+        foreach (var node in GetNodes())
+        {
+            yield return node.Value;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+    private IEnumerable<CustomLinkedListNode> GetNodes()
+    {
+        CustomLinkedListNode? current = _head;
+        while (current is not null)
+        {
+            yield return current;
+            current = current.Next;
+        }
+    }
+    private class CustomLinkedListNode
+    {
+        internal T? Value { get; }
+        internal CustomLinkedListNode? Next;
+
+        internal CustomLinkedListNode(T? value)
+        {
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return $"Value: {Value}" +
+                $"Next: {(Next is null ? "null" : Next.Value)}";
+        }
+    }
+}
+
+
